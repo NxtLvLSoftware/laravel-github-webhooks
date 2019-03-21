d9e0b76b3d9cc725e13dfc9e14c0d5abce6a2bfe
Jack Noordhuis <jackjnoordhuis@gmail.com>
2019-03-22 03:19:40 +1100

    Allow handling webhooks with no secret defined

--- src/http/middleware/VerifyGitHubWebhookSecret.php
+++ src/http/middleware/VerifyGitHubWebhookSecret.php
@@ -18,23 +18,54 @@ class VerifyGitHubWebhookSecret
 {
     public function handle(Request $request, Closure $next)
     {
+        $secret = config('github-webhooks.secret');
+        if ($secret !== '') {
+            $signature = $this->checkHeader($request);
+
+            if ($signature === false or !$this->isValidSignature($secret, $request->getContent(), $signature)) {
+                abort(Response::HTTP_UNAUTHORIZED, 'Invalid signature.');
+            }
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Make sure the signature header exists and uses the correct hash algo.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return string|bool
+     */
+    protected function checkHeader(Request $request)
+    {
         if($request->hasHeader('X-Hub-Signature')) {
             // X-Hub-Signature sha1=ff67cf27f59a9067366d653fda0baf46d1adbbdc
             $parts = explode('=', $request->header('X-Hub-Signature'));
 
             // make sure index 0 AND 1 are set and that the signature is using the correct algo
             if(isset($parts[1]) and $parts[0] === 'sha1') {
-                [$algo, $signature] = $parts;
-
-                if (hash_equals(
-                    hash_hmac('sha1', $request->getContent(), config('github-webhooks.secret')),
-                    $signature)
-                ) {
-                    return $next($request);
-                }
+                return $parts[1];
             }
         }
 
-        abort(Response::HTTP_UNAUTHORIZED, 'Invalid signature.');
+        return false;
+    }
+
+    /**
+     * Verify a signature is valid for the given payload.
+     *
+     * @param string $secret
+     * @param string $payload
+     * @param string $signature
+     *
+     * @return bool
+     */
+    protected function isValidSignature(string $secret, string $payload, string $signature): bool
+    {
+        return hash_equals(
+            hash_hmac('sha1', $payload, $secret),
+            $signature
+        );
     }
 }
